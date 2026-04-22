83a94d0f57f0432d25b0b8bee4778129ce5f31e5
ShtunderGIT <oleh.shtunder.ukr@gmail.com>
2026-04-22 04:55:19 +0000

    Wed, Apr 22, 2026, 12:55 AM -04:00

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
     true_lon = location["longitude"]
 
     # Generate and save image
-    image = get_combined_image(true_lat, true_lon, zoom=15)
+    image = get_combined_image(true_lat, true_lon, zoom=8)
     image.save("images/test_image.jpg")
 
     print("A satellite image has been saved to images/test_image.jpg")
